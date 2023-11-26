e162e6d87710e353f2fc04879f57e94422a826d0
n8th <nathanclark88@yahoo.com>
2023-11-25 22:36:50 -0600

    init

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   let previewEnabled = false;
   const backgroundOverlay = document.getElementById("background-overlay");
+  const opacitySlider = document.getElementById("opacity-slider");
+  const opacityValue = document.getElementById("opacity-value");
+  const backgroundSizeSlider = document.getElementById(
+    "background-size-slider"
+  );
+  const backgroundSizeValue = document.getElementById("background-size-value");
+
+  // Function to increase or decrease slider values by one step
+  function changeSliderValue(slider, step, increase) {
+    const currentValue = parseFloat(slider.value);
+    const newValue = increase ? currentValue + step : currentValue - step;
+    slider.value = newValue.toFixed(2); // Limit decimal places to 2
+    slider.dispatchEvent(new Event("input")); // Trigger input event to update the value
+  }
 
   // Fetching data and populating the grid
   fetch("./data.json")
@@ -39,5 +53,53 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       previewToggle.classList.remove("previewIsOn");
     }
+  });
+
+  // Event listener for the opacity slider
+  opacitySlider.addEventListener("input", function () {
+    const opacity = opacitySlider.value;
+    // Update the background opacity of the body element
+    document.body.style.backgroundColor = `rgba(255, 255, 255, ${opacity})`;
+    // Display the current opacity value to the user
+    opacityValue.textContent = opacity;
+  });
+
+  // Event listeners for increasing/decreasing opacity
+  const opacityIncreaseButton = document.getElementById("opacity-increase");
+  const opacityDecreaseButton = document.getElementById("opacity-decrease");
+  const opacityStep = 0.01;
+
+  opacityIncreaseButton.addEventListener("click", function () {
+    changeSliderValue(opacitySlider, opacityStep, true);
+  });
+
+  opacityDecreaseButton.addEventListener("click", function () {
+    changeSliderValue(opacitySlider, opacityStep, false);
+  });
+
+  // Event listener for the background size slider
+  backgroundSizeSlider.addEventListener("input", function () {
+    const backgroundSize = `${backgroundSizeSlider.value}%`;
+    // Update the background size property of the background overlay
+    backgroundOverlay.style.backgroundSize = backgroundSize;
+    // Display the current background size value to the user
+    backgroundSizeValue.textContent = backgroundSize;
+  });
+
+  // Event listeners for increasing/decreasing background size
+  const backgroundSizeIncreaseButton = document.getElementById(
+    "background-size-increase"
+  );
+  const backgroundSizeDecreaseButton = document.getElementById(
+    "background-size-decrease"
+  );
+  const backgroundSizeStep = 1;
+
+  backgroundSizeIncreaseButton.addEventListener("click", function () {
+    changeSliderValue(backgroundSizeSlider, backgroundSizeStep, true);
+  });
+
+  backgroundSizeDecreaseButton.addEventListener("click", function () {
+    changeSliderValue(backgroundSizeSlider, backgroundSizeStep, false);
   });
 });
